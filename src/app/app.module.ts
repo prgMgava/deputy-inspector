26650d01baa3720c7cc0db5a51bb8902d2b07cc3
Mgava <prgmgava@hotmail.com>
2021-11-15 14:38:42 -0300

    feat: form structured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './shared/header/header.component';
 import { FormSearchComponent } from './shared/form-search/form-search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FormSearchComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,
-    MatToolbarModule,
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FormSearchComponent,
   ],
+  imports: [AppRoutingModule, NoopAnimationsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/form-search/form-search.component.ts
+++ src/app/shared/form-search/form-search.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { State } from 'src/app/models/stateModels';
+import { states } from 'src/app/utils/states';
 
 @Component({
   selector: 'app-form-search',
   templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.css']
+  styleUrls: ['./form-search.component.css'],
 })
 export class FormSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public states: State[] = states;
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: [''],
+      siglaUf: [''],
+      siglaPartido: [''],
+      siglaSexo: [''],
+    });
+  }
+  onSubmit() {
+    const data = {
+      nome: this.form.value.nome,
+      siglaUf: this.form.value.siglaUf,
+      siglaPartido: this.form.value.siglaPartido,
+      siglaSexo: this.form.value.siglaSexo,
+    };
+    console.log(data);
   }
 
+  ngOnInit(): void {}
 }
